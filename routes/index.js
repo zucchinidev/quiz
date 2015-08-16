88c45ee4e73ec4e5e2e5ac30de27d145c92e5f7e
azga84@gmail.com <azga84@gmail.com>
2015-08-16 21:53:05 +0200

    Añadir rutas para CRUD

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,11 @@ router.param('quizId', quizController.load);
 router.get('/quizes', quizController.quizes);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 router.get('/author', quizController.author);
 
 module.exports = router;
