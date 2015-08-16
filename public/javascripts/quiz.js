190ea1ced804ae872d94603c38a0ac9942ee1b2e
azga84@gmail.com <azga84@gmail.com>
2015-08-16 21:52:44 +0200

    Mejora de los estilos

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -1,3 +1,62 @@
-/**
- * Created by andrea on 16/08/15.
- */
+/* global Quiz */
+(function() {
+  'use strict';
+
+
+  /**
+   * Clase que pretende englobar toda la funcionalidad extra y común a la aplicación que no
+   * esté resuelta por otras partes de la app
+   * @constructor
+   */
+  var Quiz = function() {
+    var that = this;
+    that.init();
+  };
+
+
+  Quiz.prototype = {
+    init: function() {
+      var that = this;
+      that.modal = document.querySelector('#modal-delete-quiz');
+      that.form = document.querySelector('#form-delete-quiz');
+      that.cancelButton = document.querySelector('#cancel-delete');
+      that.buttons = document.querySelectorAll('.btn-delete');
+      that.setEvents();
+    },
+
+
+    setEvents: function() {
+      var that = this;
+      if (that.buttons) {
+        Array.prototype.forEach.call(that.buttons, function(element) {
+          element.addEventListener('click', that.removeQuiz.bind(that, element));
+        });
+      }
+
+      that.cancelButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        that.modal.style.display = 'none';
+      });
+    },
+    removeQuiz: function(element, event) {
+      event.preventDefault();
+      var that = this;
+      var firstChild = element.parentNode.firstChild;
+      while (firstChild.nodeType !== 1) {
+        firstChild = firstChild.nextSibling;
+      }
+      var url = '/quizes/' + element.dataset.id + '?_method=delete';
+      that.form.setAttribute('action', url);
+      document.querySelector('#insert-text-question').textContent = firstChild.textContent;
+      that.modal.style.display = 'block';
+    }
+  };
+
+  window.Quiz = Quiz;
+})();
+
+window.addEventListener('load', function() {
+  'use strict';
+  var quiz = new Quiz();
+  quiz.init();
+});
