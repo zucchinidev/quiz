cecdbc7251b0824b36f6e2e4fba7a46ff6163737
azga84@gmail.com <azga84@gmail.com>
2015-08-16 21:51:54 +0200

    Añadir method-override para peticiones PUT y Delete y modificar body-parser para creación de objetos en formularios

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var partials = require('express-partials');
 var routes = require('./routes/index');
 
@@ -17,8 +18,9 @@ app.use(partials());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
