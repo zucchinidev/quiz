648f4c05484524b0a78df3f8c9963af52d681b0b
azga84@gmail.com <azga84@gmail.com>
2015-08-18 21:29:20 +0200

    Creación de acción autenticate

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,33 @@
 /**
- * Created by andrea on 18/08/15.
+ * Array que simula usuarios del sistema
+ * @type {*[]}
  */
+var users = [
+  {
+    id: 1, username: 'admin', password: '1234', rol: 'admin'
+  },
+  {
+    id: 2, username: 'pepe', password: '5678', rol: 'user'
+  }
+];
+
+// TODO: Cambiar array por users en BBDD
+
+
+/**
+ * Comprobar si las credenciales pasadas coinciden con las de algún usuario
+ * @param {string} login nombre de usuario que desea autenticarse
+ * @param {string} password del usuario que desea autenticarse
+ * @param {Function} callback que se va a ejecutar tras la comprobación.
+ */
+exports.autenticar = function(login, password, callback) {
+  'use strict';
+  var user = users.filter(function(u) {
+    return u.username === login && u.password === password;
+  })[0];
+  if (user) {
+    callback(null, user);
+  } else {
+    callback(new Error('Credenciales no válidas'));
+  }
+};
