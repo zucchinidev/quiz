14ae4fbf30b04538c74e06446dccbb523fb1d5b3
azga84@gmail.com <azga84@gmail.com>
2015-08-18 21:28:07 +0200

    Actions create, destroy, new sessions

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,57 @@
 /**
- * Created by andrea on 18/08/15.
+ * Gestiona las peticiones GET /login
+ * Si existen errores de sesión anteriores los pasa a la vista
+ * @param {{}} req objeto Request
+ * @param {{}} res objeto Response
  */
+exports.new = function(req, res) {
+  'use strict';
+  var errors = req.session.errors || {};
+  req.session.errors = {};
+  res.render('sessions/new', { errors: errors });
+};
+
+
+/**
+ * Gestiona las petiones POST /login
+ * Creación de sesión si se pasa la autentificación, almacenar en sesión al usuario actual
+ * Redirección a la acción anterior al login
+ * @param {{}} req objeto Request
+ * @param {{}} res objeto Response
+ */
+exports.create = function(req, res) {
+  'use strict';
+
+  var login = req.body.login;
+  var password = req.body.password;
+
+  var userController = require('./user_controller');
+  userController.autenticar(login, password, function(err, user) {
+    if (err) {
+      req.session.errors = [{ message: 'Se ha producido un error ' + err }];
+      res.redirect('/login');
+      return;
+    }
+
+    // Almacenamos el usuario actual en la sesión
+    req.session.user = { id: user.id, username: user.username };
+
+    res.redirect(req.session.redir.toString());
+  });
+};
+
+
+/**
+ * Gestiona las peticiones DELETE /logout
+ * Destruir sesión de usuario
+ * Redirección a la acción anterior al logout
+ * @param {{}} req objeto Request
+ * @param {{}} res objeto Response
+ */
+exports.destroy = function(req, res) {
+  'use strict';
+  if (req.session.user) {
+    delete req.session.user;
+  }
+  res.redirect(req.session.redir.toString());
+};
