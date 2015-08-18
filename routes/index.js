a9dc86ccc984a10c49986848c80d340f6cf88b39
azga84@gmail.com <azga84@gmail.com>
2015-08-18 21:43:34 +0200

    Insertar middelware de control antes de creación, modificación y borrado de quizes

--- routes/index.js
+++ routes/index.js
@@ -31,11 +31,11 @@ router.param('quizId', quizController.load);
 router.get('/quizes', quizController.quizes);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 router.get('/author', quizController.author);
 
 
