f0424848b1fdd61f9c17727ffc468f0d39ca01d1
azga84@gmail.com <azga84@gmail.com>
2015-08-18 19:10:42 +0200

    Creación del controlador de comentarios, dos acciones new(GET) y create(POST)

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,3 +1,48 @@
+/* global exports */
+
+var models = require('../models/models');
+
+
 /**
- * Created by andrea on 18/08/15.
+ * Middelware que gestiona las peticiones GET /quizes/:quizId/comments/new
+ * @param {{}} req objeto request
+ * @param {{}} res objeto request
  */
+exports.new = function(req, res) {
+  var comment = models.Comment.build({
+    text: '',
+    QuizId: req.quiz.id
+  });
+  console.log(comment);
+  res.render('comments/new', { comment: comment, quiz: req.quiz, errors: []});
+};
+
+
+/**
+ * Middelware que gestiona las peticiones POST /quizes/:quizId/comments
+ * @param {{}} req objeto request
+ * @param {{}} res objeto request
+ * @param {Function} next pasa el control al siguiente middelware
+ */
+exports.create = function (req, res, next) {
+  var comment = models.Comment.build({
+    text: req.body.comment.text,
+    QuizId: req.quiz.id
+  });
+
+  comment
+      .validate()
+      .then(function(err) {
+        if (err) {
+          res.render('comments/new', { comment: comment, quiz: req.quiz, errors: err.errors});
+        } else {
+          comment
+              .save()
+              .then(function() {
+                res.redirect('/quizes/' + comment.QuizId);
+              });
+        }
+      }).catch(function(err) {
+        next(err);
+      });
+};
