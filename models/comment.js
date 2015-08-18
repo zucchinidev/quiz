8319621b55e7e9522e4631e185eb76c1025552a9
azga84@gmail.com <azga84@gmail.com>
2015-08-18 19:08:09 +0200

    Añadir definición de la tabla Comments

--- models/comment.js
+++ models/comment.js
@@ -1,3 +1,20 @@
+/* global module */
+
+
 /**
- * Created by andrea on 18/08/15.
+ * Creación del modelo Comment
+ * @param {Sequelize} squelize
+ * @param {DataTypes} DataTypes
+ * @return {*|{}|Model}
  */
+module.exports = function(squelize, DataTypes) {
+  'use strict';
+  return squelize.define('Comment', {
+    text: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: { msg: 'No es posible crear un comentario vacío'}
+      }
+    }
+  });
+};
